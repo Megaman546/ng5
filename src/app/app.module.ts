e4dc6a7386cb776d13255697c2c9f3fee255160b
Megaman546 <soragamesultra@gmail.com>
2021-09-27 15:31:29 -0500

    add interpolation #3

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 
 import { BrowserModule } from '@angular/platform-browser';
 
+import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from '../app/home/home.component';
@@ -15,7 +16,8 @@ import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent],
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -6,10 +6,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
+  
+  itemCount!: number;
+  btnText: string = 'Add an item';
+  goalText: string = 'sscx';
+  goals = ['My first life goal','I want to climb a mountain','Go ice  skiing'];
 
   constructor() { }
 
-  ngOnInit(): void {
+  ngOnInit() {
+    this.itemCount = this.goals.length;
+  }
+
+  addItem(){
+    this.goals.push(this.goalText);
+    this.goalText='';
+    this.itemCount=this.goals.length;
   }
 
 }
